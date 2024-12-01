2837005892cc7828d77ca30310a701a046fa18aa
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-12-01 23:27:21 +0100

    feat(prettier): added prettier config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,16 @@
+export default {
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	requirePragma: false,
+	useTabs: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	semi: true,
+	tabWidth: 2,
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'avoid',
+	trailingComma: 'none',
+	printWidth: 100,
+	proseWrap: 'never',
+};
