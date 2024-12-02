916b267762ebaae162bf62df3374f4c6cb8fb376
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-12-02 15:25:22 +0100

    chore(eslint): switch to React-specific ESLint configuration for improved linting in UI package

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,4 @@
-import eslintConfig from '@repo/eslint-config';
+import eslintConfigReact from '@repo/eslint-config/react';
 
 /** @type {import("eslint").Linter.Config} */
-export default [...eslintConfig];
+export default [...eslintConfigReact];
