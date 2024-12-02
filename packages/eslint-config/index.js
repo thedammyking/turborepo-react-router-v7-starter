97d7a9b4d6d6f709d0a29e9e71619a5a6375006d
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-12-02 16:11:46 +0100

    chore(eslint): add eslint-plugin-tailwindcss for improved Tailwind CSS linting

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -7,142 +7,170 @@ import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import tsEslint from 'typescript-eslint';
 
+import path from 'node:path';
+
+import tailwind from 'eslint-plugin-tailwindcss';
+
+const project = path.resolve(process.cwd(), 'tsconfig.json');
+
+const tailwindConfig = path.resolve(process.cwd(), 'tailwind.config.ts');
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	// Ignore
-	{
-		ignores: ['build', 'node_modules', 'public', 'dist'],
-	},
+  // Ignore
+  {
+    ignores: ['build', 'node_modules', 'public', 'dist']
+  },
 
-	// Default
-	{
-		files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node },
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-		},
-	},
+  // Default
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    }
+  },
 
-	// JavaScript
-	pluginJs.configs.recommended,
-	{
-		files: ['**/*.{js,mjs,cjs}'],
-		rules: {
-			'no-console': 'warn',
-			'array-callback-return': 'error',
-			'consistent-return': 'off',
-			'no-await-in-loop': 'error',
-			'no-constructor-return': 'error',
-			'no-duplicate-imports': 'error',
-			'no-promise-executor-return': 'error',
-			'no-self-compare': 'error',
-			'no-unmodified-loop-condition': 'error',
-			'no-use-before-define': 'error',
-			'no-useless-assignment': 'error',
-			'block-scoped-var': 'error',
-			yoda: 'error',
-			'prefer-promise-reject-errors': 'warn',
-			'prefer-template': 'warn',
-		},
-	},
+  // JavaScript
+  pluginJs.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    rules: {
+      'no-console': 'warn',
+      'array-callback-return': 'error',
+      'consistent-return': 'off',
+      'no-await-in-loop': 'error',
+      'no-constructor-return': 'error',
+      'no-duplicate-imports': 'error',
+      'no-promise-executor-return': 'error',
+      'no-self-compare': 'error',
+      'no-unmodified-loop-condition': 'error',
+      'no-use-before-define': 'error',
+      'no-useless-assignment': 'error',
+      'block-scoped-var': 'error',
+      yoda: 'error',
+      'prefer-promise-reject-errors': 'warn',
+      'prefer-template': 'warn'
+    }
+  },
 
-	// TypeScript
-	...tsEslint.configs.recommended,
-	{
-		files: ['**/*.{ts,jsx,tsx}'],
-		languageOptions: {
-			parser: tsParser,
-			parserOptions: {
-				project: './tsconfig.json',
-			},
-		},
-		rules: {
-			'@typescript-eslint/consistent-type-imports': [
-				'warn',
-				{ prefer: 'type-imports' },
-			],
-			'@typescript-eslint/no-non-null-assertion': 'error',
-			'@typescript-eslint/no-explicit-any': 'error',
-		},
-	},
+  // TypeScript
+  ...tsEslint.configs.recommended,
+  {
+    files: ['**/*.{ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-explicit-any': 'error'
+    }
+  },
 
-	// Simple Import Sort
-	{
-		plugins: {
-			'simple-import-sort': simpleImportSort,
-		},
-		rules: {
-			'simple-import-sort/exports': 'error',
-			'simple-import-sort/imports': [
-				'error',
-				{
-					groups: [
-						// Packages `react` related packages come first.
-						['^react', '^@?\\w'],
-						// Node.js builtins prefixed with `node:`.
-						['^node:'],
-						// Side effect imports.
-						['^\\u0000'],
-						// Packages.
-						// Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-						['^@?\\w'],
-						// Internal packages with `@`, `~`, `#` alias.
-						['^(@|~|#)(/.*|$)'],
-						// Anything not matched in another group.
-						['^'],
-						// Parent imports. Put `..` last.
-						['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-						// Other relative imports. Put same-folder imports and `.` last.
-						['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-						// Style imports.
-						['^.+\\.?(css|scss|less|styl|sass)$'],
-					],
-				},
-			],
-		},
-	},
+  // Simple Import Sort
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            ['^react', '^@?\\w'],
+            // Node.js builtins prefixed with `node:`.
+            ['^node:'],
+            // Side effect imports.
+            ['^\\u0000'],
+            // Packages.
+            // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+            ['^@?\\w'],
+            // Internal packages with `@`, `~`, `#` alias.
+            ['^(@|~|#)(/.*|$)'],
+            // Anything not matched in another group.
+            ['^'],
+            // Parent imports. Put `..` last.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Style imports.
+            ['^.+\\.?(css|scss|less|styl|sass)$']
+          ]
+        }
+      ]
+    }
+  },
 
-	// Unused Imports
-	{
-		plugins: {
-			'unused-imports': unusedImports,
-		},
-		rules: {
-			'no-unused-vars': 'off',
-			'@typescript-eslint/no-unused-vars': 'off',
-			'unused-imports/no-unused-imports': 'error',
-			'unused-imports/no-unused-vars': [
-				'error',
-				{
-					vars: 'all',
-					varsIgnorePattern: '^_',
-					args: 'after-used',
-					argsIgnorePattern: '^_',
-					caughtErrorsIgnorePattern: '^_',
-				},
-			],
-		},
-	},
+  // Unused Imports
+  {
+    plugins: {
+      'unused-imports': unusedImports
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_'
+        }
+      ]
+    }
+  },
 
-	// Import
-	importX.flatConfigs.recommended,
-	importX.flatConfigs.typescript,
-	{
-		rules: {
-			'import-x/default': 'off',
-			'import-x/extensions': 'off',
-			'import-x/no-anonymous-default-export': 'off',
-			'import-x/no-extraneous-dependencies': 'error',
-			'import-x/no-unresolved': 'off',
-			'import-x/no-mutable-exports': 'error',
-			'import-x/first': 'error',
-			'import-x/newline-after-import': 'error',
-			'import-x/no-duplicates': 'error',
-			'import-x/no-named-as-default-member': 'off',
-		},
-	},
+  // Import
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    rules: {
+      'import-x/default': 'off',
+      'import-x/extensions': 'off',
+      'import-x/no-anonymous-default-export': 'off',
+      'import-x/no-extraneous-dependencies': 'error',
+      'import-x/no-unresolved': 'off',
+      'import-x/no-mutable-exports': 'error',
+      'import-x/first': 'error',
+      'import-x/newline-after-import': 'error',
+      'import-x/no-duplicates': 'error',
+      'import-x/no-named-as-default-member': 'off'
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project
+        },
+        node: {
+          extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx']
+        }
+      }
+    }
+  },
 
-	// Prettier
-	configPrettier,
+  // Tailwind
+  ...tailwind.configs['flat/recommended'],
+  {
+    settings: {
+      tailwindcss: {
+        callees: ['cn', 'classnames', 'clsx', 'ctl'],
+        config: tailwindConfig,
+        removeDuplicates: true
+      }
+    }
+  },
+
+  // Prettier
+  configPrettier
 ];
