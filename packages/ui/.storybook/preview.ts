3133478835eeb99575484b2f8f133c36bfb34498
Oluwadamilola Babalola <kingdammie@gmail.com>
2024-12-04 18:47:17 +0100

    feat(ui): integrate Storybook and update configurations
    - Added Storybook support by including necessary dependencies and configurations in package.json.
    - Updated ESLint configuration to include storybook plugin for improved linting.
    - Modified TypeScript configuration to recognize storybook types.
    - Removed obsolete vitest configuration and test setup files.
    - Enhanced button component with display name for better debugging.
    
    This commit enhances the UI package with Storybook integration, streamlining the development process and improving component documentation.

---
+++ packages/ui/.storybook/preview.ts
@@ -0,0 +1,27 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
+import type { Preview } from '@storybook/react';
+
+import '../src/styles/main.scss';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    }
+  }
+};
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark'
+    },
+    defaultTheme: 'light'
+  })
+];
+
+export default preview;
